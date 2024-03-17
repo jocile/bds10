52c851379deccdcde29c668678107d6359cb32cf
jocile <jocile@gmail.com>
2024-03-17 20:28:58 -0300

    📄 Adcionando campos do form

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -1,13 +1,26 @@
 import { useHistory } from 'react-router-dom';
 import './styles.css';
+import { Controller, useForm } from 'react-hook-form';
+import { Employee } from 'types/employee';
+import Select from 'react-select';
+import { Department } from 'types/department';
+import { useState } from 'react';
 
 const Form = () => {
 
   const history = useHistory();
 
+  const {
+    control,
+    register,
+    formState: { errors },
+  } = useForm<Employee>();
+
   const handleCancel = () => {
     history.push('/admin/employees');
   };
+
+  const [selectDepartment, setSelectDepartment] = useState<Department[]>([]);
 
   return (
     <div className="employee-crud-container">
@@ -19,23 +32,67 @@ const Form = () => {
             <div className="col employee-crud-inputs-left-container">
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+              <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do funcionário"
+                  name="name"
+                  data-testid="name"
                 />
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                {errors.name?.message}
                 </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+              <input
+                  {...register('email', {
+                    required: 'Campo obrigatório',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Email inválido',
+                    },
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.email ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Email do funcionário"
+                  name="email"
+                  data-testid="email"
                 />
                 <div className="invalid-feedback d-block">
-                  
+                {errors.email?.message}
                 </div>
               </div>
-
+              <div className="margin-bottom-30">
+                <label htmlFor="department" className="d-none">Departamento</label>
+                <Controller 
+                  name="department"
+                  rules={{required: true}}
+                  control={control}
+                  render={({field}) => (
+                    <Select {...field}
+                      options={selectDepartment}
+                      isClearable
+                      classNamePrefix="employee-crud-select"
+                      getOptionLabel={(department: Department) => department.name}
+                      getOptionValue={(department: Department) => String(department.id)}
+                      inputId="department"
+                    />  
+                  )}
+                />
+                {errors.department && (
+                  <div className="invalid-feedback d-block">
+                    Campo obrigatório
+                  </div>
+                )}
+              </div>
             </div>
           </div>
           <div className="employee-crud-buttons-container">
